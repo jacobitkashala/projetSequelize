ac9156247a5d1c421397dbb7853e12c87256e9f0
jacobitkashala <jacobitkashala@gmail.com>
2021-08-18 15:24:48 +0200

    add:model patient

--- models/patient.js
+++ models/patient.js
@@ -14,7 +14,25 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Patient.init({
+
+    nom_patient: DataTypes.STRING,
+    prenom_patient: DataTypes.STRING,
+    postnom_patient: DataTypes.STRING,
+    sexe_patient: DataTypes.STRING,
+    age_patient: DataTypes.INTEGER,
+    provenance_patient: DataTypes.STRING,
+    mode_arrive: DataTypes.STRING,
+    poids_naissance: DataTypes.FLOAT,
+    telephone: DataTypes.STRING,
+    avatar: DataTypes.TEXT,
+    adresse: DataTypes.STRING,
+    date_naissance: DataTypes.DATE,
+    id_Cause_malnutrition: DataTypes.INTEGER,
+
+    id_famille: DataTypes.INTEGER,
+
     id_patient: DataTypes.STRING
+
   }, {
     sequelize,
     modelName: 'Patient',
